375d11999299818600e79f31b386b7481c2e0475
keloriane <kevin.flbt@gmail.com>
2019-11-28 17:42:21 +0100

    stag untrack files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Gallery from './components/Gallery/Gallery';
+import Cursor from "./components/Cursor/Cursor"
+import Info from './components/Info/Info';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="App">
+      <Cursor />
+      <Router>
+        <Switch>
+          <Route exact path="/info" component={Info} />
+          <Route path="/slider" component={Gallery} />
+        </Switch>
+      </Router>
     </div>
   );
 }
