195e975fae4ac2cf1152b57e9e16e406622db76d
keloriane <kevin.flbt@gmail.com>
2019-12-05 15:42:40 +0100

    Updates

---
+++ precache-manifest.93cb3c4944663edb26f85c6a7da3fd01.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a373bf4a17eda0a536bed7ef5bb7c607",
+    "url": "/keloriane/gyuto-app/index.html"
+  },
+  {
+    "revision": "35eb7d3ef724249a0eef",
+    "url": "/keloriane/gyuto-app/static/css/main.980b46a0.chunk.css"
+  },
+  {
+    "revision": "719e27d3e30c549fc0bc",
+    "url": "/keloriane/gyuto-app/static/js/2.421bbe69.chunk.js"
+  },
+  {
+    "revision": "35eb7d3ef724249a0eef",
+    "url": "/keloriane/gyuto-app/static/js/main.459ade12.chunk.js"
+  },
+  {
+    "revision": "d1d729428b6d02735c40",
+    "url": "/keloriane/gyuto-app/static/js/runtime-main.1e6a435f.js"
+  },
+  {
+    "revision": "d2f2aa2c955b37677890cbfe52ce0ecc",
+    "url": "/keloriane/gyuto-app/static/media/gyuto.d2f2aa2c.svg"
+  },
+  {
+    "revision": "1106b46b8d97483333a397d2198ed01d",
+    "url": "/keloriane/gyuto-app/static/media/image1.1106b46b.jpg"
+  },
+  {
+    "revision": "38afd55f9e4bd22e129bf770d7605a06",
+    "url": "/keloriane/gyuto-app/static/media/image10.38afd55f.jpg"
+  },
+  {
+    "revision": "539634da13a1195e2b5545765f8023a3",
+    "url": "/keloriane/gyuto-app/static/media/image11.539634da.jpg"
+  },
+  {
+    "revision": "c7d90da52f4cc841bd5fae4087e7b110",
+    "url": "/keloriane/gyuto-app/static/media/image12.c7d90da5.jpg"
+  },
+  {
+    "revision": "e72d940b1de7be05a807edcce60a1b23",
+    "url": "/keloriane/gyuto-app/static/media/image15.e72d940b.jpg"
+  },
+  {
+    "revision": "db887942c4d0f1df10ffb8620b5e5785",
+    "url": "/keloriane/gyuto-app/static/media/image16.db887942.jpg"
+  },
+  {
+    "revision": "92f9a771cec8ec0bfea5429bd02a6f22",
+    "url": "/keloriane/gyuto-app/static/media/image17.92f9a771.jpg"
+  },
+  {
+    "revision": "466be98e12bbc2d3509cbf8066307ae8",
+    "url": "/keloriane/gyuto-app/static/media/image19.466be98e.jpg"
+  },
+  {
+    "revision": "d5143522b7d5250b8e8ccd892a662a49",
+    "url": "/keloriane/gyuto-app/static/media/image2.d5143522.jpg"
+  },
+  {
+    "revision": "629fc9b7cff342fa33b6b57e10860f74",
+    "url": "/keloriane/gyuto-app/static/media/image22.629fc9b7.jpg"
+  },
+  {
+    "revision": "1782d343169d4afb792ea0f83d82417b",
+    "url": "/keloriane/gyuto-app/static/media/image27.1782d343.jpg"
+  },
+  {
+    "revision": "7fb10fe938255d09deeeb029a20adfb5",
+    "url": "/keloriane/gyuto-app/static/media/image29.7fb10fe9.jpg"
+  },
+  {
+    "revision": "bd23a87df06a05078375f82bcace3d0c",
+    "url": "/keloriane/gyuto-app/static/media/image30.bd23a87d.jpg"
+  },
+  {
+    "revision": "5ed63fe9939098a31bd8f2b2b544eefd",
+    "url": "/keloriane/gyuto-app/static/media/image32.5ed63fe9.jpg"
+  },
+  {
+    "revision": "f2a9d46d8ed5cf5165d605435fdbacbd",
+    "url": "/keloriane/gyuto-app/static/media/image33.f2a9d46d.jpg"
+  },
+  {
+    "revision": "6c9bfb72fbbc2e37f5367031a731ff91",
+    "url": "/keloriane/gyuto-app/static/media/image42.6c9bfb72.jpg"
+  },
+  {
+    "revision": "65af1358975ee60b27a0bc73626b2ba4",
+    "url": "/keloriane/gyuto-app/static/media/leftArrow.65af1358.svg"
+  },
+  {
+    "revision": "fc4decb36f0865cdf3ad4756427bc8c7",
+    "url": "/keloriane/gyuto-app/static/media/rightArrow.fc4decb3.svg"
+  },
+  {
+    "revision": "9ed617ab3a051bec25dbc2f8a9b2f7dc",
+    "url": "/keloriane/gyuto-app/static/media/tcrcbyn1.9ed617ab.ttf"
+  }
+]);
